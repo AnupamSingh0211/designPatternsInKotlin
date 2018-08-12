de5d64261d3de7f7baa07abcb949667b0814cb0b
Hitanshu Dhawan <hitanshudhawan@urbanclap.com>
2018-08-12 16:46:41 +0530

    added example for State Design Pattern

---
+++ app/src/main/java/com/hala/designpatternsinkotlin/Behavioral/State/ATMMachine.kt
@@ -0,0 +1,52 @@
+package com.hala.designpatternsinkotlin.Behavioral.State
+
+class ATMMachine {
+
+    val yesCardState: ATMState
+    val noCardState: ATMState
+    val hasPinState: ATMState
+    val noCashState: ATMState
+
+    internal var atmState: ATMState
+
+    internal var cashInMachine = 2000
+
+    init {
+        yesCardState = HasCard(this)
+        noCardState = NoCard(this)
+        hasPinState = HasPin(this)
+        noCashState = NoCash(this)
+
+        atmState = noCardState
+
+        if (cashInMachine <= 0) {
+            atmState = noCashState
+        }
+    }
+
+    fun setATMState(newATMState: ATMState) {
+        atmState = newATMState
+    }
+
+    fun setCashInMachine(newCashInMachine: Int) {
+        cashInMachine = newCashInMachine
+    }
+
+    // these 4 functions will be called on there respective states...
+    fun insertCard() {
+        atmState.insertCard()
+    }
+
+    fun ejectCard() {
+        atmState.ejectCard()
+    }
+
+    fun requestCash(cashToWithdraw: Int) {
+        atmState.requestCash(cashToWithdraw)
+    }
+
+    fun insertPin(pinEntered: Int) {
+        atmState.insertPin(pinEntered)
+    }
+
+}
---
+++ app/src/main/java/com/hala/designpatternsinkotlin/Behavioral/State/HasCard.kt
@@ -0,0 +1,28 @@
+package com.hala.designpatternsinkotlin.Behavioral.State
+
+class HasCard(private val atmMachine: ATMMachine) : ATMState {
+
+    override fun insertCard() {
+        println("You can only insert one card at a time")
+    }
+
+    override fun ejectCard() {
+        println("Your card is ejected")
+        atmMachine.setATMState(atmMachine.noCardState)
+    }
+
+    override fun requestCash(cashToWithdraw: Int) {
+        println("You have not entered your PIN")
+    }
+
+    override fun insertPin(pinEntered: Int) {
+        if (pinEntered == 1234) {
+            println("You entered the correct PIN")
+            atmMachine.setATMState(atmMachine.hasPinState)
+        } else {
+            println("You entered the wrong PIN")
+            println("Your card is ejected")
+            atmMachine.setATMState(atmMachine.noCardState)
+        }
+    }
+}
